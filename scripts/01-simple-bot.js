4e7a8e23e951874e40c73c8965a67937437731c2
Erth2000 <maddasallhell@gmail.com>
2025-10-04 18:21:07 -0500

    Update 01-simple-bot.js

--- scripts/01-simple-bot.js
+++ scripts/01-simple-bot.js
@@ -2,8 +2,8 @@ const mineflayer = require('mineflayer');
 
 // Setup bot connection
 const bot = mineflayer.createBot({
-  host: 'localhost',
-  port: '12345',
-  username: "Hello_world",
-  version: '1.8.9'
+  host: 'ErthSMP2.aternos.me',
+  port: '32299',
+  username: "Erthsbot",
+  version: '1.21.9'
 });
